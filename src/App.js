06a341a87306e8bc11d8419f2da217ed4c8cd4c9
Th4run <tharun.th4run@gmail.com>
2023-12-09 16:50:59 +0530

    Your commit message here

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+
+
+/**
+ * Header
+ * -Logo
+ * -Navitems
+ * Body
+ * -Search
+ * -Restaurant_container
+ *   -Restaurant_details
+ * Footer
+ * -Contact
+ * -Links
+ * -Address
+ * -Copyright
+ */
+
+
+const Header = () => {
+  return(
+    <div className='header'>
+      <div className='logo-container'><img className="logo" src='https://png.pngtree.com/png-vector/20221121/ourmid/pngtree-shopefood-logo-png-image_6472274.png'/></div>
+      <div className='navitems'>
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+           <Header/>
     </div>
   );
 }
